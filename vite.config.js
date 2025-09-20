9a30a580913c2bd69bcaba66ed318a3e95f2dd0a
handyuja <handyuja@navr.com>
2025-09-20 19:40:41 +0900

    fix: set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base : '/handyuja.github.io/',
+  base: '/exercise-guide/', // 리포지토리명과 동일
 })
